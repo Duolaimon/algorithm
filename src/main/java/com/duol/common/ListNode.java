cd7eeffd42a50efaff5280dcd306877728bfa0c9
何嘉庚 <hejiageng@baijiahulian.com>
2020-01-28 20:32:25 +0800

    2

--- src/main/java/com/duol/common/ListNode.java
+++ src/main/java/com/duol/common/ListNode.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- *
- * Copyright (c) 2018 Wei SHEN 
-
+ * <p>
+ * Copyright (c) 2018 Wei SHEN
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
-
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
-
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,16 +23,31 @@
  */
 package com.duol.common;
 
+import com.google.common.base.Objects;
+
 /**
  * ListNode is Singly-Linked List
  * ex: 1->2->3->4->5
- * 
+ *
  * @author Wei SHEN
  */
 public class ListNode {
     public int val;
     public ListNode next;
-    public ListNode(int x) { val = x; }
+
+    public ListNode(int x) {
+        val = x;
+    }
+
+    public static ListNode newListNode(int... data) {
+        ListNode first = new ListNode(data[0]);
+        ListNode node = first;
+        for (int i = 1; i < data.length; i++) {
+            node.next = new ListNode(data[i]);
+            node = node.next;
+        }
+        return first;
+    }
 
     public String toString() {
         ListNode cursor = this;
@@ -42,8 +57,23 @@ public class ListNode {
             cursor = cursor.next;
         } while (cursor != null);
         int length = sb.length();
-        if (length > 2) { sb = sb.delete(length-2,length); }
+        if (length > 2) {
+            sb = sb.delete(length - 2, length);
+        }
         return sb.toString();
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ListNode node = (ListNode) o;
+        return val == node.val &&
+                Objects.equal(next, node.next);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(val, next);
+    }
 }
---
+++ src/main/java/com/duol/leetcode/add_two_numbers/Solution2.java
@@ -0,0 +1,23 @@
+/**
+ * Leetcode - add_two_numbers
+ */
+package com.duol.leetcode.add_two_numbers;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+        return null;
+    }
+
+}
