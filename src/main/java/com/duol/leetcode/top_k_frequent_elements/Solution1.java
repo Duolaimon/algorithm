bc636088cdf69f49297d17bb492e7475ec365d43
何嘉庚 <hejiageng@baijiahulian.com>
2020-02-24 18:57:37 +0800

    347

---
+++ src/main/java/com/duol/leetcode/top_k_frequent_elements/Solution1.java
@@ -0,0 +1,47 @@
+/**
+ * Leetcode - top_k_frequent_elements
+ */
+package com.duol.leetcode.top_k_frequent_elements;
+
+import java.util.*;
+
+import com.duol.common.*;
+
+/**
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ * if (log.isDebugEnabled()) {
+ * log.debug("a + b = {}", sum);
+ * }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        // build hash map : character and how often it appears
+        HashMap<Integer, Integer> count = new HashMap<>();
+        for (int n : nums) {
+            count.put(n, count.getOrDefault(n, 0) + 1);
+        }
+
+        // init heap 'the less frequent element first'
+        PriorityQueue<Integer> heap =
+                new PriorityQueue<>(Comparator.comparingInt(count::get));
+
+        // keep k top frequent elements in the heap
+        for (int n : count.keySet()) {
+            heap.add(n);
+            if (heap.size() > k)
+                heap.poll();
+        }
+
+        // build output list
+        List<Integer> top_k = new LinkedList<>();
+        while (!heap.isEmpty())
+            top_k.add(heap.poll());
+        Collections.reverse(top_k);
+        return top_k;
+    }
+
+}
