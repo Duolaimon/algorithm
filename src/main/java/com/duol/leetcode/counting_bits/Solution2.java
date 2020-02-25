ad026c6c03692d44f573e7f7ba5eb1c3b60c23ba
何嘉庚 <hejiageng@baijiahulian.com>
2020-02-25 10:35:37 +0800

    338

---
+++ src/main/java/com/duol/leetcode/counting_bits/Solution2.java
@@ -0,0 +1,32 @@
+/**
+ * Leetcode - counting_bits
+ */
+package com.duol.leetcode.counting_bits;
+
+import java.util.*;
+
+import com.duol.common.*;
+
+/**
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ * if (log.isDebugEnabled()) {
+ * log.debug("a + b = {}", sum);
+ * }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    public int[] countBits(int num) {
+        if (num <= 0) {
+            return new int[1];
+        }
+        int[] res = new int[num + 1];
+        for (int i = 0; i < num + 1; i++) {
+            res[i] = res[i >> 1] + (i & 1);
+        }
+        return res;
+    }
+
+}
