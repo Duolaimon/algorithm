c3a6649ef4552b74d4f27632b201f211302a7fa8
何嘉庚 <hejiageng@baijiahulian.com>
2020-03-31 09:35:06 +0800

    面试题62. 圆圈中最后剩下的数字

---
+++ src/main/java/com/duol/leetcode/last_number_remaining_in_the_circle/Solution1.java
@@ -0,0 +1,29 @@
+/**
+ * Leetcode - last_number_remaining_in_the_circle
+ */
+package com.duol.leetcode.last_number_remaining_in_the_circle;
+
+import java.util.*;
+
+import com.duol.common.*;
+
+/**
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ * if (log.isDebugEnabled()) {
+ * log.debug("a + b = {}", sum);
+ * }
+ * =============================================
+ */
+class Solution1 implements Solution {
+    public int lastRemaining(int n, int m) {
+        int ans = 0;
+        // 最后一轮剩下2个人，所以从2开始反推
+        for (int i = 2; i <= n; i++) {
+            ans = (ans + m) % i;
+        }
+        return ans;
+    }
+
+}
---
+++ src/main/java/com/duol/leetcode/last_number_remaining_in_the_circle/Solution2.java
@@ -0,0 +1,23 @@
+/**
+ * Leetcode - last_number_remaining_in_the_circle
+ */
+package com.duol.leetcode.last_number_remaining_in_the_circle;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    public int lastRemaining(int n, int m){
+        return 0;
+    }
+
+}
