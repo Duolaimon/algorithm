16ee3db3ccce54e2bf97cea05f6edfd3fdc05d03
何嘉庚 <hejiageng@baijiahulian.com>
2020-04-08 11:09:28 +0800

    面试题 01.07. 旋转矩阵

---
+++ src/main/java/com/duol/leetcode/rotate.matrix.LCCI/Solution1.java
@@ -0,0 +1,41 @@
+/**
+ * Leetcode - rotate.matrix.LCCI
+ */
+package com.duol.leetcode.rotate.matrix.LCCI;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    public void rotate(int[][] matrix){
+        int n = matrix.length;
+        // 先以对角线（左上-右下）为轴进行翻转
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+        // 再对每一行以中点进行翻转
+        int mid = n >> 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < mid; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = tmp;
+            }
+        }
+
+    }
+
+}
