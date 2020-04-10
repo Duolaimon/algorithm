cf2caf2dd150f35660230622c5f2abfd1bead141
何嘉庚 <hejiageng@baijiahulian.com>
2020-04-10 09:36:20 +0800

    22. 括号生成

---
+++ src/main/java/com/duol/leetcode/generate.parentheses/Solution1.java
@@ -0,0 +1,29 @@
+/**
+ * Leetcode - generate.parentheses
+ */
+package com.duol.leetcode.generate.parentheses;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    public List<String> generateParenthesis(int n){
+        List<String> parenthesis = new LinkedList<>();
+
+        return parenthesis;
+    }
+
+    private void generateParenthesis(List<String> parenthesis, int n) {
+
+    }
+
+}
---
+++ src/main/java/com/duol/leetcode/generate.parentheses/Solution2.java
@@ -0,0 +1,39 @@
+/**
+ * Leetcode - generate.parentheses
+ */
+package com.duol.leetcode.generate.parentheses;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    List<String> res = new ArrayList<>();
+    public List<String> generateParenthesis(int n) {
+        dfs(n, n, "");
+        return res;
+    }
+
+    private void dfs(int left, int right, String curStr) {
+        if (left == 0 && right == 0) { // 左右括号都不剩余了，递归终止
+            res.add(curStr);
+            return;
+        }
+
+        if (left > 0) { // 如果左括号还剩余的话，可以拼接左括号
+            dfs(left - 1, right, curStr + "(");
+        }
+        if (right > left) { // 如果右括号剩余多于左括号剩余的话，可以拼接右括号
+            dfs(left, right - 1, curStr + ")");
+        }
+    }
+
+}
