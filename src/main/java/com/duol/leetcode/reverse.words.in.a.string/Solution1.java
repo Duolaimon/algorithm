9318c800dd9e71aa58cbc427ce6d1390b5c09c19
何嘉庚 <hejiageng@baijiahulian.com>
2020-04-10 23:51:24 +0800

    151. 翻转字符串里的单词

---
+++ src/main/java/com/duol/leetcode/reverse.words.in.a.string/Solution1.java
@@ -0,0 +1,28 @@
+/**
+ * Leetcode - reverse.words.in.a.string
+ */
+package com.duol.leetcode.reverse.words.in.a.string;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    public String reverseWords(String s){
+        // 除去开头和末尾的空白字符
+        s = s.trim();
+        // 正则匹配连续的空白字符作为分隔符分割
+        List<String> wordList = Arrays.asList(s.split("\\s+"));
+        Collections.reverse(wordList);
+        return String.join(" ", wordList);
+    }
+
+}
--- src/main/java/exam/KuaiShou.java
+++ src/main/java/exam/KuaiShou.java
@@ -118,4 +118,26 @@ public class KuaiShou {
         int sValue = (sEnd - sStart + 1) * same;
         return Math.max(cValue, sValue);
     }
+
+    public int getPos(int[] A, int val) {
+        int n = A.length;
+        if (n == 0) return -1;
+        if (n == 1) return 0;
+        int high = n - 1;
+        int low = 0;
+        while (low < high) {
+            int mid = low + (high - low) / 2; //防止溢出
+            if (val == A[mid]) {
+                high = mid;   //如果找到了val,则继续对low~mid段数据进行二分查找
+            } else if (val > A[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        if (A[high] == val) { //如果存在val，则high对应的值就是第一次出现的位置
+            return high;
+        }
+        return -1;
+    }
 }
