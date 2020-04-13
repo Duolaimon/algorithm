9a3dc58c6981717910dd8090492591c6097f90da
何嘉庚 <hejiageng@baijiahulian.com>
2020-04-13 23:07:29 +0800

    面试题 16.03. 交点

---
+++ src/main/java/com/duol/leetcode/intersection_lcci/Solution2.java
@@ -0,0 +1,23 @@
+/**
+ * Leetcode - intersection_lcci
+ */
+package com.duol.leetcode.intersection_lcci;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    public double[] intersection(int[] start1, int[] end1, int[] start2, int[] end2){
+        return null;
+    }
+
+}
