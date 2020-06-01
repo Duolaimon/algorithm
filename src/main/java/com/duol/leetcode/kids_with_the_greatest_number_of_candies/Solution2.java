6abebc56a778f56137b087a8e961da768b892a64
何嘉庚 <hejiageng@baijiahulian.com>
2020-06-01 20:47:19 +0800

    1431

---
+++ src/main/java/com/duol/leetcode/kids_with_the_greatest_number_of_candies/Solution2.java
@@ -0,0 +1,23 @@
+/**
+ * Leetcode - kids_with_the_greatest_number_of_candies
+ */
+package com.duol.leetcode.kids_with_the_greatest_number_of_candies;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies){
+        return null;
+    }
+
+}
--- src/main/java/com/duol/leetcode/minimum_window_substring/Solution.java
+++ src/main/java/com/duol/leetcode/minimum_window_substring/Solution.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * No.76
  * 给你一个字符串 S、一个字符串 T，请在字符串 S 里面找出：包含 T 所有字符的最小子串。
  *
  * 示例：
