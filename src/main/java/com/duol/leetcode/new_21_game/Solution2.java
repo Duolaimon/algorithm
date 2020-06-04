fda7f873826ae0f40d95a38fae98d383e394db13
何嘉庚 <hejiageng@baijiahulian.com>
2020-06-04 20:49:34 +0800

    837

---
+++ src/main/java/com/duol/leetcode/new_21_game/Solution2.java
@@ -0,0 +1,23 @@
+/**
+ * Leetcode - new_21_game
+ */
+package com.duol.leetcode.new_21_game;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    public double new21Game(int N, int K, int W){
+        return 0;
+    }
+
+}
