1df654b4f6ff7ae2fd8c36563127d475a14fd799
何嘉庚 <hejiageng@baijiahulian.com>
2020-06-10 20:10:08 +0800

    9. 回文数

---
+++ src/main/java/com/duol/leetcode/palindrome_number/Solution1.java
@@ -0,0 +1,35 @@
+/**
+ * Leetcode - palindrome_number
+ */
+package com.duol.leetcode.palindrome_number;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    public boolean isPalindrome(int x){
+        if (x < 0) {
+            return false;
+        }
+        String s = String.valueOf(x);
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+}
---
+++ src/main/java/com/duol/leetcode/palindrome_number/Solution2.java
@@ -0,0 +1,23 @@
+/**
+ * Leetcode - palindrome_number
+ */
+package com.duol.leetcode.palindrome_number;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    public boolean isPalindrome(int x){
+        return true;
+    }
+
+}
