8744629d9ef5a36a2ca889e75a43a12f2093454d
何嘉庚 <hejiageng@baijiahulian.com>
2020-06-10 09:34:41 +0800

    面试题46. 把数字翻译成字符串

---
+++ src/main/java/com/duol/leetcode/num_translate_2_string/Solution1.java
@@ -0,0 +1,24 @@
+/**
+ * Leetcode - num_translate_2_string
+ */
+package com.duol.leetcode.num_translate_2_string;
+
+import java.util.*;
+
+import com.duol.common.*;
+
+/**
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ * if (log.isDebugEnabled()) {
+ * log.debug("a + b = {}", sum);
+ * }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    public int translateNum(int num) {
+        return 0;
+    }
+}
