d1573675eb9297ffbbb4c41e7f3c0f00122f6644
何嘉庚 <hejiageng@baijiahulian.com>
2020-06-13 23:04:23 +0800

    5420. 商品折扣后的最终价格

---
+++ src/main/java/com/duol/leetcode/final_prices/Solution1.java
@@ -0,0 +1,38 @@
+/**
+ * Leetcode - final_prices
+ */
+package com.duol.leetcode.final_prices;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    public int[] finalPrices(int[] prices){
+        int[] result = new int[prices.length];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        int index = 1;
+        while (!stack.isEmpty() && index < prices.length) {
+            while (!stack.isEmpty() && prices[index] <= prices[stack.peek()]) {
+                Integer t = stack.pop();
+                result[t] = prices[t] - prices[index];
+            }
+            stack.push(index++);
+        }
+        while (!stack.isEmpty()) {
+            Integer t = stack.pop();
+            result[t] = prices[t];
+        }
+        return result;
+    }
+
+}
---
+++ src/main/java/com/duol/leetcode/find_two_non_overlapping_sub_arrays_each_with_target_sum/Solution2.java
@@ -0,0 +1,23 @@
+/**
+ * Leetcode - find_two_non_overlapping_sub_arrays_each_with_target_sum
+ */
+package com.duol.leetcode.find_two_non_overlapping_sub_arrays_each_with_target_sum;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    public int minSumOfLengths(int[] arr, int target){
+        return null;
+    }
+
+}
