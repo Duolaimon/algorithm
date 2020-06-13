6c9045f46ee42e228dd1051d06a40530dce97508
何嘉庚 <hejiageng@baijiahulian.com>
2020-06-13 20:21:42 +0800

    70. 爬楼梯

---
+++ src/main/java/com/duol/leetcode/climbing_stairs/Solution1.java
@@ -0,0 +1,32 @@
+/**
+ * Leetcode - climbing_stairs
+ */
+package com.duol.leetcode.climbing_stairs;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    public int climbStairs(int n){
+        if (n == 0) {
+            return 0;
+        }
+        int l = 0, r = 1;
+        for (int i = 1; i <= n; i++) {
+            int swap = r;
+            r = l + r;
+            l = swap;
+        }
+        return r;
+    }
+
+}
---
+++ src/main/java/com/duol/leetcode/climbing_stairs/Solution2.java
@@ -0,0 +1,23 @@
+/**
+ * Leetcode - climbing_stairs
+ */
+package com.duol.leetcode.climbing_stairs;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    public int climbStairs(int n){
+        return 0;
+    }
+
+}
