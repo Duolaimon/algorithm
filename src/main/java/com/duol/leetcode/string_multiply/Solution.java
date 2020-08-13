9806cdac0c09ce3829476ded59de7a560cdffc20
何嘉庚 <hejiageng@baijiahulian.com>
2020-08-13 20:58:19 +0800

    43. 字符串相乘

---
+++ src/main/java/com/duol/leetcode/string_multiply/Solution.java
@@ -0,0 +1,38 @@
+/**
+* Leetcode - string_multiply
+*/
+package com.duol.leetcode.string_multiply;
+import java.util.*;
+import com.duol.common.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 43. 字符串相乘
+ *
+ * 给定两个以字符串形式表示的非负整数num1和num2，返回num1和num2的乘积，它们的乘积也表示为字符串形式。
+ *
+ * 示例 1:
+ *
+ * 输入: num1 = "2", num2 = "3"
+ * 输出: "6"
+ * 示例2:
+ *
+ * 输入: num1 = "123", num2 = "456"
+ * 输出: "56088"
+ * 说明：
+ *
+ * num1和num2的长度小于110。
+ * num1 和num2 只包含数字0-9。
+ * num1 和num2均不以零开头，除非是数字 0 本身。
+ * 不能使用任何标准库的大数类型（比如 BigInteger）或直接将输入转换为整数来处理。
+ *
+ */
+interface Solution {
+
+// use this Object to print the log (call from slf4j facade)
+static Logger log = LoggerFactory.getLogger(Solution.class);
+
+    String multiply(String num1, String num2);
+}
