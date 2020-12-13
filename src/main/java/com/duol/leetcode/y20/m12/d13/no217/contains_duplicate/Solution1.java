ef8eb885a6518ef5255546171b6d971aa0da3792
何嘉庚 <hejiageng@baijiahulian.com>
2020-12-13 19:53:45 +0800

    217. 存在重复元素

---
+++ src/main/java/com/duol/leetcode/y20/m12/d13/no217/contains_duplicate/Solution1.java
@@ -0,0 +1,29 @@
+/**
+ * Leetcode - contains_duplicate
+ */
+package com.duol.leetcode.y20.m12.d13.no217.contains_duplicate;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    public boolean containsDuplicate(int[] nums){
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ src/main/java/com/duol/leetcode/y20/m12/d13/no217/contains_duplicate/Solution2.java
@@ -0,0 +1,29 @@
+/**
+ * Leetcode - contains_duplicate
+ */
+package com.duol.leetcode.y20.m12.d13.no217.contains_duplicate;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int x : nums) {
+            if (!set.add(x)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
