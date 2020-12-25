1c76d02abb821b74e3f9d550f15a0145e0c5a702
何嘉庚 <hejiageng@baijiahulian.com>
2020-12-25 16:39:17 +0800

    3. 无重复字符的最长子串

---
+++ src/main/java/com/duol/leetcode/y20/m12/d25/no3/longest_substring_without_repeating_characters/Solution1.java
@@ -0,0 +1,37 @@
+/**
+ * Leetcode - longest_substring_without_repeating_characters
+ */
+package com.duol.leetcode.y20.m12.d25.no3.longest_substring_without_repeating_characters;
+
+import java.util.*;
+
+import com.duol.common.*;
+
+/**
+ * 记录之前出现过的字符
+ */
+class Solution1 implements Solution {
+
+    public int lengthOfLongestSubstring(String s) {
+        // 哈希集合，记录每个字符是否出现过
+        Set<Character> occ = new HashSet<>();
+        int n = s.length();
+        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        int rk = -1, ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                // 左指针向右移动一格，移除一个字符
+                occ.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                // 不断地移动右指针
+                occ.add(s.charAt(rk + 1));
+                ++rk;
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = Math.max(ans, rk - i + 1);
+        }
+        return ans;
+    }
+
+}
---
+++ src/main/java/com/duol/leetcode/y20/m12/d25/no3/longest_substring_without_repeating_characters/Solution2.java
@@ -0,0 +1,38 @@
+/**
+ * Leetcode - longest_substring_without_repeating_characters
+ */
+package com.duol.leetcode.y20.m12.d25.no3.longest_substring_without_repeating_characters;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    public int lengthOfLongestSubstring(String s) {
+        int[] last = new int[128];
+        for(int i = 0; i < 128; i++) {
+            last[i] = -1;
+        }
+        int n = s.length();
+
+        int res = 0;
+        int start = 0; // 窗口开始位置
+        for(int i = 0; i < n; i++) {
+            int index = s.charAt(i);
+            start = Math.max(start, last[index] + 1);
+            res   = Math.max(res, i - start + 1);
+            last[index] = i;
+        }
+
+        return res;
+    }
+
+}
