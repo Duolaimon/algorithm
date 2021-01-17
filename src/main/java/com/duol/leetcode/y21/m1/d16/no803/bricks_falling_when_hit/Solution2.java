012197af9bc1b576104c76ee3ecab274b5c856de
何嘉庚 <hejiageng@baijia.com>
2021-01-17 10:21:08 +0800

    803. 打砖块

---
+++ src/main/java/com/duol/leetcode/y21/m1/d16/no803/bricks_falling_when_hit/Solution2.java
@@ -0,0 +1,23 @@
+/**
+ * Leetcode - bricks_falling_when_hit
+ */
+package com.duol.leetcode.y21.m1.d16.no803.bricks_falling_when_hit;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    public int[] hitBricks(int[][] grid, int[][] hits){
+        return null;
+    }
+
+}
