6f7fdc2b5711e9081d8c389f18cbe5a1c119d91a
何嘉庚 <hejiageng@baijia.com>
2021-01-18 20:15:11 +0800

    721. 账户合并

---
+++ src/main/java/com/duol/leetcode/y21/m1/d18/no721/accounts_merge/Solution2.java
@@ -0,0 +1,23 @@
+/**
+ * Leetcode - accounts_merge
+ */
+package com.duol.leetcode.y21.m1.d18.no721.accounts_merge;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    public List<List<String>> accountsMerge(List<List<String>> accounts){
+        return null;
+    }
+
+}
