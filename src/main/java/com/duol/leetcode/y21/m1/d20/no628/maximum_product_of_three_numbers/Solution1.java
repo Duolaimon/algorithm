1b8619544d752de2fdcdc7cb067d14ee30d0ee67
何嘉庚 <hejiageng@baijia.com>
2021-01-21 10:16:38 +0800

    628. 三个数的最大乘积

---
+++ src/main/java/com/duol/leetcode/y21/m1/d20/no628/maximum_product_of_three_numbers/Solution1.java
@@ -0,0 +1,27 @@
+/**
+ * Leetcode - maximum_product_of_three_numbers
+ */
+package com.duol.leetcode.y21.m1.d20.no628.maximum_product_of_three_numbers;
+
+import java.util.*;
+
+import com.duol.common.*;
+
+/**
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ * if (log.isDebugEnabled()) {
+ * log.debug("a + b = {}", sum);
+ * }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return Math.max(nums[0] * nums[1] * nums[n - 1], nums[n - 3] * nums[n - 2] * nums[n - 1]);
+    }
+
+}
---
+++ src/main/java/com/duol/leetcode/y21/m1/d20/no628/maximum_product_of_three_numbers/Solution2.java
@@ -0,0 +1,23 @@
+/**
+ * Leetcode - maximum_product_of_three_numbers
+ */
+package com.duol.leetcode.y21.m1.d20.no628.maximum_product_of_three_numbers;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    public int maximumProduct(int[] nums){
+        return 0;
+    }
+
+}
