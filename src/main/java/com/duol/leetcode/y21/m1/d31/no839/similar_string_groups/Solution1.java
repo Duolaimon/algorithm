6bcc69d56bc9f1b5c8a56fb93a7229ca88ced085
何嘉庚 <hejiageng@baijia.com>
2021-02-01 09:38:40 +0800

    839. 相似字符串组

---
+++ src/main/java/com/duol/leetcode/y21/m1/d31/no839/similar_string_groups/Solution1.java
@@ -0,0 +1,65 @@
+/**
+ * Leetcode - similar_string_groups
+ */
+package com.duol.leetcode.y21.m1.d31.no839.similar_string_groups;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    int[] f;
+
+    public int numSimilarGroups(String[] strs) {
+        int n = strs.length;
+        int m = strs[0].length();
+        f = new int[n];
+        for (int i = 0; i < n; i++) {
+            f[i] = i;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int fi = find(i), fj = find(j);
+                if (fi == fj) {
+                    continue;
+                }
+                if (check(strs[i], strs[j], m)) {
+                    f[fi] = fj;
+                }
+            }
+        }
+        int ret = 0;
+        for (int i = 0; i < n; i++) {
+            if (f[i] == i) {
+                ret++;
+            }
+        }
+        return ret;
+    }
+
+    public int find(int x) {
+        return f[x] == x ? x : (f[x] = find(f[x]));
+    }
+
+    public boolean check(String a, String b, int len) {
+        int num = 0;
+        for (int i = 0; i < len; i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                num++;
+                if (num > 2) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
---
+++ src/main/java/com/duol/leetcode/y21/m1/d31/no839/similar_string_groups/Solution2.java
@@ -0,0 +1,23 @@
+/**
+ * Leetcode - similar_string_groups
+ */
+package com.duol.leetcode.y21.m1.d31.no839.similar_string_groups;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    public int numSimilarGroups(String[] strs){
+        return 0;
+    }
+
+}
