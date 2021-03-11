634cc54485b26a5a485cc684fe33fded1cb275bf
何嘉庚 <hejiageng@baijia.com>
2021-03-11 10:22:04 +0800

    224. 基本计算器

---
+++ src/main/java/com/duol/leetcode/y21/m3/d10/no224/basic_calculator/Solution2.java
@@ -0,0 +1,23 @@
+/**
+ * Leetcode - basic_calculator
+ */
+package com.duol.leetcode.y21.m3.d10.no224.basic_calculator;
+import java.util.*;
+import com.duol.common.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    public int calculate(String s){
+        return 1;
+    }
+
+}
